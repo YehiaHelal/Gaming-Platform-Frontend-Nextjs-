a781c903afbe495f56988cbb82c46f96e62013a0
Yehia Helal <mangetsuhozuki287@yahoo.com>
2023-06-17 18:00:39 +0300

    62

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -522,7 +522,7 @@ const LoginPage = () => {
             "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
             "Access-Control-Allow-Headers":
               "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            "Content-Type": "application/json",
+            // "Content-Type": "application/json",
             // "Content-Type": "multipart/form-data",
           },
           // headers: {
